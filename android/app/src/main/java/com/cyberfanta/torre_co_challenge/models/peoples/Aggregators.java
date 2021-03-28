d3a60f30273c77fa29644de5b2f8c9def3fe1361
Julio César León <julioleon2004@gmail.com>
2021-03-27 21:11:10 -0400

    Initial commit - All json loaded in java class to start the UI design

---
+++ android/app/src/main/java/com/cyberfanta/torre_co_challenge/models/peoples/Aggregators.java
@@ -0,0 +1,11 @@
+package com.cyberfanta.torre_co_challenge.models.peoples;
+
+public class Aggregators{
+
+	@Override
+ 	public String toString(){
+		return 
+			"Aggregators{" + 
+			"}";
+		}
+}
---
+++ android/app/src/main/kotlin/com/cyberfanta/torre_co_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cyberfanta.torre_co_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
